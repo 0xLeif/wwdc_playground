c93e6778e35d3e93657fe3e25a8c14443709151a
Zach Eriksen <zeriksen@clientresourcesinc.com>
2020-06-25 08:36:25 -0500

    Init playground

---
+++ Shared/wwdc_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  wwdc_playgroundApp.swift
+//  Shared
+//
+//  Created by CRi on 6/24/20.
+//
+
+import SwiftUI
+
+@main
+struct wwdc_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
